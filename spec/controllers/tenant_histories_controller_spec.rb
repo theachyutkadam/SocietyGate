20005e0b1620be5f29a5ba4d62444f3b2b62b6c9
ak <theachyutkadam@gmail.com>
2023-04-16 12:21:29 +0530

    Rename saffold

--- spec/controllers/tenant_histories_controller_spec.rb
+++ spec/controllers/tenant_histories_controller_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe Api::TenantHistoriesController, type: :controller do
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
-  # TenantHistoriesController. Be sure to keep this updated too.
+  # TenentHistoriesController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
   describe "GET #index" do
